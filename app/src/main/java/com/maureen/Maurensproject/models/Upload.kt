fd2f7788e20cb24350772b2018f10d1a2a2b7a1b
john <mberamaureen53@gmail.com>
2024-05-13 08:28:14 +0300

    initial commit

---
+++ app/src/main/java/com/maureen/Maurensproject/models/Upload.kt
@@ -0,0 +1,24 @@
+package com.maureen.Maurensproject.models
+
+class Upload {
+    class Upload {
+        class Upload{
+            var name:String=""
+            var quantity:String=""
+            var price:String=""
+            var imageUrl:String=""
+            var id:String=""
+
+            constructor(name:String,quantity:String,price:String,imageUrl:String,id:String){
+
+                this.name=name
+                this.quantity=quantity
+                this.price=price
+                this.imageUrl=imageUrl
+                this.id=id
+
+            }
+            constructor()
+        }
+    }
+}
